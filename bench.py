1b9d43ca5261452fe70764997d9600cdabfb581e
mensinda <daniel@mensinger-ka.de>
2016-12-14 00:22:26 +0100

    Improved bench.py

--- bench.py
+++ bench.py
@@ -2,7 +2,7 @@
 
 import swigLib
 
-iterations = 1000000
+iterations = 10000000
 
 sw = swigLib.Test()
 sw.printHeadder( "SWIG" )
@@ -10,28 +10,35 @@ sw.pureCPPBench( iterations )
 
 sw_cla = swigLib.ClassA()
 
-print( "\nPython / SWIG test ( %s )" % "{:,}".format( iterations ) )
+print( "\nPython / SWIG test ({:,} iterations; nanoseconds)".format( iterations ) )
 
 sw.startBench()
 for i in range( iterations ):
   sw.testFN1( 1, i )
 res = sw.stopBench()
 
-print( "  ==> testFN1: %s" % "{:,}".format( res ) )
+print( "  ==> testFN1: {:,}".format( res ) )
 
 sw.startBench()
 for i in range( iterations ):
   sw.testFN2( sw_cla )
 res = sw.stopBench()
 
-print( "  ==> testFN3: %s" % "{:,}".format( res ) )
+print( "  ==> testFN3: {:,}".format( res ) )
 
 sw.startBench()
 for i in range( iterations ):
   sw.testFN3( sw_cla )
 res = sw.stopBench()
 
-print( "  ==> testFN3: %s" % "{:,}".format( res ) )
+print( "  ==> testFN3: {:,}".format( res ) )
+
+sw.startBench()
+for i in range( iterations ):
+  continue
+res = sw.stopBench()
+
+print( "\n\nEmpty python loop with {:,} iterations takes {:,} ns".format(iterations, res) )
 
 del sw_cla
 del sw
